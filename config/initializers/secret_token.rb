0d49e8256a28e81331b7ee0431da2de3e527283d
BitGub <shaundev.gordon@gmail.com>
2013-08-27 16:16:21 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zucono::Application.config.secret_key_base = '8e88683d5933d90b136a5489bf9729ebcd158818e71695176e73f2383c8e8e878172621458a81b171757feb82e04fae4352f3138c9f4b7017f4044c486202fb5'
