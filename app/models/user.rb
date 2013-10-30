4fc7be8141cb06f371e4408ba24c774c2487f867
BitGub <shaundev.gordon@gmail.com>
2013-10-30 15:03:41 +0000

    invalid email format(GREEN)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
   validates :first_name, :surname, presence: true
-  validates :email, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
 end
