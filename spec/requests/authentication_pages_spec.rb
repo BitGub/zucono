dbb555ea9c6775c746381a4905998854b73eb33d
BitGub <shaundev.gordon@gmail.com>
2013-10-30 17:41:01 +0000

    invalid sign(RED)

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -10,4 +10,15 @@ describe "AuthenticationPages" do
     it { should have_content("Sign in") }
     it { should have_title("Sign in") }
   end 
+  
+  describe "signin" do
+    before { visit root_path }
+
+    describe "with invalid information" do
+      before { click_button "Sign in" }
+
+      it { should have_title('Sign in') }
+      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+    end
+  end
 end
