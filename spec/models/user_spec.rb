99ab22b0573df732efb547ca49f0808267ddf853
BitGub <shaundev.gordon@gmail.com>
2013-10-30 15:27:47 +0000

    password and confirmation(RED)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
   describe User do
     
-    before { @user = User.new(first_name: "Example", surname: "User", email: "user@example.com") }
+    before { @user = User.new(first_name: "Example", surname: "User", email: "user@example.com", password: "foobar", password_confirmation: "foobar") }
 
     subject { @user }
 
@@ -10,6 +10,8 @@ require 'spec_helper'
     it { should respond_to(:surname) }
     it { should respond_to(:email) }
     it { should respond_to(:password_digest) }
+    it { should respond_to(:password) }
+    it { should respond_to(:password_confirmation) }
     
     it { should be_valid }
     
@@ -46,6 +48,19 @@ require 'spec_helper'
         user_with_same_email.save
       end
 
+      it { should_not be_valid }
+    end
+    
+    describe "when password is not present" do
+      before do
+        @user = User.new(name: "Example User", email: "user@example.com",
+                         password: " ", password_confirmation: " ")
+      end
+      it { should_not be_valid }
+    end
+    
+    describe "when password doesn't match confirmation" do
+      before { @user.password_confirmation = "mismatch" }
       it { should_not be_valid }
     end
   end
