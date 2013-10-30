4e0017ae19756f5bbbf53bae39c9ba5271d768e5
BitGub <shaundev.gordon@gmail.com>
2013-10-30 13:56:29 +0000

    respond_to name(RED)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
-describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  describe User do
+    
+    before { @user = User.new(first_name: "Example", surname: "User", email: "user@example.com") }
+
+    subject { @user }
+
+    it { should respond_to(:first_name) }
+    it { should respond_to(:surname) }
+    it { should respond_to(:email) }
+    
+    it { should be_valid }
+    
+    describe "when first name is not present" do
+        before { @user.first_name = " " }
+        it { should_not be_valid }
+      end
+  end
