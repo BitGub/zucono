6cb9952c928694af652c9691e069d4ed10c1e994
BitGub <shaundev.gordon@gmail.com>
2013-10-30 18:18:04 +0000

    initial successful sign in tests(not actually testing sign in yet)(RED)

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -20,5 +20,20 @@ describe "AuthenticationPages" do
       it { should have_title('Sign in') }
       it { should have_selector('div.alert.alert-error', text: 'Invalid') }
     end
+    
+    describe "with valid information" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        fill_in "Email",    with: user.email.upcase
+        fill_in "Password", with: user.password
+        click_button "Sign in"
+      end
+
+        it { should have_content("Hello, #{user.name}") }
+        it { should have_link('Products',    href: products_path) }
+        it { should have_link('Stocks',      href: stocks_path) }
+        it { should have_link('Orders',      href: orders_path) }
+        it { should have_link('Sign out',    href: signout_path) }
+    end
   end
 end
