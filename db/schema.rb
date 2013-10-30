9b559ddf1006e817c54044d3ea92d058fc646ea7
BitGub <shaundev.gordon@gmail.com>
2013-10-30 13:38:09 +0000

    add users model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130918142530) do
+ActiveRecord::Schema.define(version: 20131030133332) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,5 +34,14 @@ ActiveRecord::Schema.define(version: 20130918142530) do
   end
 
   add_index "stocks", ["product_id"], name: "index_stocks_on_product_id", using: :btree
+
+  create_table "users", force: true do |t|
+    t.string   "first_name"
+    t.string   "surname"
+    t.string   "email"
+    t.integer  "role_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
