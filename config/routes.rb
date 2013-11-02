faf1b1c1b484906c098f97bd3be4c9723ed8be54
BitGub <shaundev.gordon@gmail.com>
2013-11-02 21:55:47 +0000

    RED on Admin::users#new

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Zucono::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  
   namespace :admin do
     resources :users
-    get :dashboard
+    get "dashboard"
   end
   resources :users
   resources :orders
--- spec/requests/admin_user_pages_spec.rb
+++ spec/requests/admin_user_pages_spec.rb
@@ -8,9 +8,16 @@ describe "AdminUserPages" do
     let(:user) { FactoryGirl.create(:admin_user) }
     before(:each) do
       sign_in user
-      visit admin_dashboard_path(user)
+      visit admin_dashboard_path
     end
+  
+  it { should have_title('Dashboard') }
+  
   end
   
-  it { should have_title("Dashboard") }
+  describe "new user page" do
+    before { visit new_admin_user_path }
+    
+    it { should have_title('New User') }
+  end
 end
