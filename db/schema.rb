4c64019e4af9097f08c2a3d5b6008a76bc0d9817
BitGub <shaundev.gordon@gmail.com>
2013-11-12 15:23:29 +0000

    add supplier id to products table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131112151919) do
+ActiveRecord::Schema.define(version: 20131112152234) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20131112151919) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "minimum_stock"
+    t.integer  "supplier_id"
   end
 
   create_table "roles", force: true do |t|
