6f1a0fd107dbc2fe276918cb1e467cfcde9c5dea
BitGub <shaundev.gordon@gmail.com>
2013-11-12 15:22:09 +0000

    add supplier id to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131112134047) do
+ActiveRecord::Schema.define(version: 20131112151919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20131112134047) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.integer  "supplier_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
