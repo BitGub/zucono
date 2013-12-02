e9746b00c60cd96f99b6c8e6b4df70171dd7a855
BitGub <shaundev.gordon@gmail.com>
2013-12-02 19:25:04 +0000

    deleted unused files

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -1,14 +1,3 @@
 $(document).ready(function() {
 	
-	// form_index = new Array();
-	// 
-	// $('td > input').each(function() {
-	// 	form_index.push($(this).attr('value').substr($(this).attr('value').length - 1));
-	//    });
-	//    
-	// $("#SubmitStockForm").on("click", function() {
-	// 	
-	// 	alert(form_index)
-	// 	
-	//    	});
  });
--- app/views/products/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@products) do |product|
-  json.extract! product, :product_name, :unit_price, :product_description, :deleted
-  json.url product_url(product, format: :json)
-end
