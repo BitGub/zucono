453b606c688ec23b7fe9839fedd6896c91a700c2
BitGub <shaundev.gordon@gmail.com>
2013-12-02 19:41:24 +0000

    added users#index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,10 @@ class UsersController < ApplicationController
   def admin_dashboard
   end
   
+  def index
+    @users = User.all
+  end
+  
   def new
     @user = User.new
   end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,6 @@
 module UsersHelper
+  
+  def full_name(user)
+    user.first_name + " " + user.surname
+  end
 end
